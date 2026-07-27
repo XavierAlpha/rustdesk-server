852fda76959a278fc7de750033cff292c63309af
XavierAlpha <52537840+XavierAlpha@users.noreply.github.com>
2026-07-28 01:53:27 +0800

    deploy: harden container and Kubernetes runtimes

--- docker/rootfs/usr/bin/healthcheck.sh
+++ docker/rootfs/usr/bin/healthcheck.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-/package/admin/s6/command/s6-svstat /run/s6-rc/servicedirs/hbbr || exit 1
-/package/admin/s6/command/s6-svstat /run/s6-rc/servicedirs/hbbs || exit 1
+set -eu
+
+/usr/bin/rustdesk-utils healthcheck 127.0.0.1:21117
+/usr/bin/rustdesk-utils healthcheck 127.0.0.1:21116
